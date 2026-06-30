eeec72b24c8daa95772380cff59389e549193bed
Facephi <developer@facephi.com>
2026-06-30 11:31:50 +0200

    Promote FPHINfcWidget SPM 0.3.8

--- Package.swift
+++ Package.swift
@@ -58,8 +58,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "FPHINfcWidget",
-            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHINfcWidget/0.3.7/FPHINfcWidget.zip",
-            checksum: "3013c5670557587259f116a54262040bcf61e52a2a2e056be39cd0f5e06a3b6b"
+            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHINfcWidget/0.3.8/FPHINfcWidget.zip",
+            checksum: "d54d552f236b0e833ff3939f18a5d3d8e7680fa9e412f9decd081cd1f4fc0d92"
         ),
     ]
 )
